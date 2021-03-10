e97844d93f20d12f440a4eb90ef9deeb3c88b4bc
Daniel Cunha <danielcunha@MacBook-Pro.local>
2021-03-10 19:16:27 -0300

    reports_generator

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,18 +1,31 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
 
-  @doc """
-  Hello world.
+  def build(filename) do
+    # case File.read("reports/#{filename}") do
+    #   {:ok, result} -> result
+    #   {:error, reason} -> reason
+    # end
+    # "reports/#{filename}"
+    # |> File.read()
+    # |> handle_file()
+    # "reports/#{filename}"
+    # |> File.stream!()
+    # |> Enum.map(fn line -> parse_line(line) end)
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.reduce(%{}, fn line, report ->
+      [id, food_name, price] = parse_line(line)
+      Map.put(report, id, price)
+    end)
+  end
 
-  ## Examples
+  defp handle_file({:ok, file_content}), do: file_content
+  defp handle_file({:error, _reason}), do: "Error while opening file!"
 
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
   end
 end
