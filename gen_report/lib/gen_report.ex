816a45ec1f76b2484f88bb7fe584fb2ef97a6f46
Daniel Cunha <danielcunha@MacBook-Pro.local>
2021-03-16 21:13:46 -0300

    2nd Challenge

--- gen_report/lib/gen_report.ex
+++ gen_report/lib/gen_report.ex
@@ -1,18 +1,57 @@
 defmodule GenReport do
-  @moduledoc """
-  Documentation for `GenReport`.
-  """
+  alias GenReport.Parser
 
-  @doc """
-  Hello world.
+  def build(file_name) do
+    {:ok, res} =
+      file_name
+      |> Parser.parse_file()
 
-  ## Examples
+    result = Enum.reduce(res, report_acc(), fn line, report ->
+        get_values(line, report)
+      end)
 
-      iex> GenReport.hello()
-      :world
+    {:ok, result}
+  end
 
-  """
-  def hello do
-    :world
+  def build(), do: {:error, "invalid file!"}
+
+  # nome, horas trabalhadas, dia, mês e ano
+  defp get_values(
+         [name, worked_hours, _day, month, year],
+         %{
+           :all_hours => all_hours,
+           :hours_per_month => hours_per_month,
+           :hours_per_year => hours_per_year
+         } = report
+       ) do
+    all_hours =
+      Map.merge(all_hours, %{name => worked_hours}, fn _key, val1, val2 -> val1 + val2 end)
+
+    hours_per_month =
+      Map.merge(hours_per_month, %{name => %{month => worked_hours}}, fn _key, val1, val2 ->
+        Map.merge(val1, val2, fn _key, vval1, vval2 -> vval1 + vval2 end)
+      end)
+
+    hours_per_year =
+      Map.merge(hours_per_year, %{name => %{year => worked_hours}}, fn _key, val1, val2 ->
+        Map.merge(val1, val2, fn _key, vval1, vval2 -> vval1 + vval2 end)
+      end)
+
+    %{
+      report
+      | :all_hours => all_hours,
+        :hours_per_month => hours_per_month,
+        :hours_per_year => hours_per_year
+    }
+  end
+
+  defp report_acc(), do: create_report(%{}, %{}, %{})
+
+  defp create_report(all_hours, hours_per_month, hours_per_year) do
+    %{
+      :all_hours => all_hours,
+      :hours_per_month => hours_per_month,
+      :hours_per_year => hours_per_year
+    }
   end
 end
